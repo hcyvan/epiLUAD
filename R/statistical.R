40b24836c27d30dc7c896b144216cb9f322b63f0
Navy <navy@126.com>
2022-10-08 13:12:27 +0800

    update

--- R/statistical.R
+++ R/statistical.R
@@ -56,3 +56,51 @@ target<-table(data)
 chisq.test(target)
 print(target)
 
+
+#----------------------------------------------------------------------------------------------------------------------
+# Table S5. The distribution of SR-DMCs in genome
+#----------------------------------------------------------------------------------------------------------------------
+SRDMC<-loadData('SRDMC')
+SRDMC <- GRanges(SRDMC)
+SRDMCList<-split(SRDMC, SRDMC$class)
+options(ChIPseeker.ignore_1st_exon = T)
+options(ChIPseeker.ignore_1st_intron = T)
+options(ChIPseeker.ignore_downstream = T)
+options(ChIPseeker.ignore_promoter_subcategory = T)
+peakAnnoList<- lapply(SRDMCList, annotatePeak, tssRegion=c(-5000, 3000),TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene)
+parseAnno<- function(gr){
+  stat<-gr@annoStat
+  stat$num<-gr@annoStat$Frequency*gr@peakNum/100
+  stat
+}
+eHyperDmc<-parseAnno(peakAnnoList$`Early-Hyper-DMC`)
+eHypoDmc<-parseAnno(peakAnnoList$`Early-Hypo-DMC`)
+lHyperDmc<-parseAnno(peakAnnoList$`Late-Hyper-DMC`)
+lHypoDmc<-parseAnno(peakAnnoList$`Late-Hypo-DMC`)
+tab<-data.frame(earlyHyper=eHyperDmc$num, earlyHypo=eHypoDmc$num, lateHyper=lHyperDmc$num, lateHypo=lHypoDmc$num)
+rownames(tab)<-eHyperDmc$Feature
+chisq.test(tab)
+
+#----------------------------------------------------------------------------------------------------------------------
+# Table S6. The distribution of SR-DMRs in genome
+#----------------------------------------------------------------------------------------------------------------------
+SRDMR<-loadData('SRDMR')
+SRDMR <- GRanges(SRDMR)
+SRDMRList<-split(SRDMR, SRDMR$class)
+options(ChIPseeker.ignore_1st_exon = T)
+options(ChIPseeker.ignore_1st_intron = T)
+options(ChIPseeker.ignore_downstream = T)
+options(ChIPseeker.ignore_promoter_subcategory = T)
+peakAnnoList<- lapply(SRDMRList, annotatePeak, tssRegion=c(-5000, 3000),TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene)
+parseAnno<- function(gr){
+  stat<-gr@annoStat
+  stat$num<-gr@annoStat$Frequency*gr@peakNum/100
+  stat
+}
+eHyperDmr<-parseAnno(peakAnnoList$`Early-Hyper-DMR`)
+eHypoDmr<-parseAnno(peakAnnoList$`Early-Hypo-DMR`)
+lHyperDmr<-parseAnno(peakAnnoList$`Late-Hyper-DMR`)
+lHypoDmr<-parseAnno(peakAnnoList$`Late-Hypo-DMR`)
+tab<-data.frame(earlyHyper=eHyperDmr$num, earlyHypo=eHypoDmr$num, lateHyper=lHyperDmr$num, lateHypo=lHypoDmr$num)
+rownames(tab)<-eHyperDmc$Feature
+chisq.test(tab)
